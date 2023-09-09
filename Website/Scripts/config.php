1a6be016fcc53b6bec36d0108629202801f55969
GhostMan15 <46796299+GhostMan15@users.noreply.github.com>
2023-09-09 11:58:34 +0200

    Update config.php

--- Website/Scripts/config.php
+++ Website/Scripts/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'test');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'smv');
 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
