2c70a5cb7d3326f3c16633f3c6bfb309bd0068bc
Zanono <80070344+Zanono@users.noreply.github.com>
2023-09-12 11:58:49 +0200

    Add files via upload

---
+++ Website/Misc/Website/login.php
@@ -0,0 +1,132 @@
+<?php
+include("Scripts/config.php");
+//START THE SESSION
+session_start();
+
+$_SESSION["login_error"] = "";
+$error = "";
+$allgood = true;
+
+//EXECUTE ON SUBMITTING OF FORM 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_escape_string($db, $_POST["username"]);
+    $password = mysqli_escape_string($db, $_POST["password"]);
+
+    //Check if the credentials are empty
+    if($username == "" || $password == ""){
+        $error = "Vaše uporabniško ime / geslo ne mora biti prazno.";
+        $allgood = false;
+    }
+
+    //if all is good, execute query to check credentials
+    if($allgood){
+        //query for a user with the given credentials
+        $sql1 = "SELECT * FROM `user` WHERE `username` = '$username' AND `geslo` = '$password'";
+        $result1 = mysqli_query($db, $sql1);
+        $row = mysqli_fetch_assoc($result1);
+        $count = mysqli_num_rows($result1);
+
+        //If there is a result, allow login and store user data in session
+        if($count == 1){
+            $_SESSION["id"] = $row["id_user"];
+            $_SESSION["username"] = $username;
+            $_SESSION["user_type"] = $row["user_type"];
+            header("location: home.php");
+        }
+
+        //if there is no result, deny access
+        else{
+            $error = "Vaše uporabniško ime / geslo se ne ujemata.";
+            $allgood = false;
+            $_SESSION["login_error"] = $error;
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!--HEAD-->
+
+<head>
+    <script src="Scripts/login.js"></script>
+    <meta name="author" content="Mark Sadnik">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Stylesheets/login.css">
+    <title>Login to Ultis</title>
+</head>
+<!--HEAD-->
+
+<!--BODY-->
+
+<body>
+    <!--MAIN-->
+    <form method="post" action="login.php" class="main">
+
+        <!--ICON-->
+        <div class="top">
+            <div class="img_con">
+                <img src="Pictures/logo.png" class="logo">
+            </div>
+        </div>
+        <!--ICON-->
+
+        <!--FORM-->
+        <div class="form middle">
+            <div class="username">
+                <div class="field_title" id="user_title">
+                    Uporabniško ime
+                </div>
+                <div>
+                    <input type="text" name="username" class="field" id="user_field" required onblur="delete_spaces()">
+                </div>
+            </div>
+            <div class="password">
+                <div class="field_title" id="pass_title">
+                    <div class="pass_head">
+                        Geslo
+                    </div>
+                    <div class="show_con">
+                        <button type="button" class="show_button" id="show_btn" onclick="click_show_button()">Show</button>
+                    </div>
+                </div>
+                <div>
+                    <input type="password" name="password" id="pass_field" class="field" required onblur="delete_spaces()">
+                </div>
+            </div>
+        </div>
+        <!--FORM-->
+
+        <!--BUTON-->
+        <div class="bottom">
+            <div class="error" id="error_login"></div>
+            <div>
+                <input type="submit" class="submit_btn" value="Login">
+            </div>
+        </div>
+        <!--BUTTON-->
+
+    </form>
+    <!--MAIN-->
+
+</body>
+<!--BODY-->
+
+</html>
+
+<?php
+//print out error if there is any
+if(isset($_SESSION["login_error"])){
+    $error = $_SESSION["login_error"];
+    echo "
+        <script>
+            let error_msg = '$error';
+            let error_item = document.getElementById('error_login');
+            
+            error_item.innerHTML = error_msg;
+        </script>
+    ";
+}
+?>
---
+++ Website/Misc/Website/vp.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vaš profil</title>
+    <link rel="stylesheet" href="Stylesheets/style.css">
+</head>
+<body>
+
+<div class="bannerVP">
+        <div class="navbar">
+            <a href="homepage.html"><img src="Pictures/logo2.png" class="logo" ></a>
+            <ul>
+                <li> <a href="home.php">Domov</a></li>
+                <li> <a href="$$$">Moduli</a></li>
+                <li> <a href="$$$">Predmeti</a></li>
+                <li> <a href="vp.php">Vaš Profil</a></li>
+                <li> <a href="login.php">Odjava</a></li>
+            </ul>
+        </div>
+
+        <div class="contentVP"> <h1> Vaš Profil </h1><br><br>
+            
+                                Ime: xxx <img src= "Pictures/edit.png" class="edit"></img><br><br>
+                                Password: *** <img src= "Pictures/edit.png" class="edit"></img><br><br>
+                                O meni:  <img src= "Pictures/edit.png" class="edit"></img><br><br>
+                                sngojgsdkogjoghjgldhkg </div>
+
+       
+    </div>
+    
+</body>
+</html>
