b6eb398aea4edd4a610d60688dff410f108f7513
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2023-09-12 18:58:10 +0200

    Ustvaru class.php, navbar.css

---
+++ Website/class.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--HEAD-->
+
+<head>
+    <meta name="author" content="Mark Sadnik">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Stylesheets/class.css">
+    <link rel="stylesheet" type="text/css" href="Stylesheets/navbar.css">
+    <title>Predmeti</title>
+</head>
+<!--HEAD-->
+
+<!--BODY-->
+
+<body>
+    <div class="banner">
+        <div class="navbar">
+            <a href='homepage.html'><img src='Pictures/logo1.png' class='logo'></a>
+            <ul>
+                <li> <a href='home.php'>Domov</a></li>
+                <li> <a href='class.php'>Predmeti</a></li>
+                <li> <a href='$$$'>Učenci</a></li>
+                <li> <a href='vp.php'>Vaš Profil</a></li>
+                <li> <a href='login.php'>Odjava</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+<!--BODY-->
+
+</html>
--- Website/vp.php
+++ Website/vp.php
@@ -1,17 +1,37 @@
+<?php
+include("Scripts/config.php");
+//START THE SESSION
+session_start();
+
+//if the user isn't logged in / session variables aren't set, redirect to login
+if (!isset($_SESSION["id"], $_SESSION["username"], $_SESSION["user_type"])) {
+    header("location: login.php");
+}
+//if the user is logged in, allow access
+else {
+    $username = $_SESSION["username"];
+    $id = $_SESSION["id"];
+    $user_type =  $_SESSION["user_type"];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vaš profil</title>
-    <link rel="stylesheet" href="Stylesheets/style.css">
+    <link rel="stylesheet" type="text/css" href="Stylesheets/style1.css">
+    <link rel="stylesheet" type="text/css" href="Stylesheets/navbar.css">
 </head>
+
 <body>
 
-<div class="bannerVP">
+    <div class="bannerVP">
         <div class="navbar">
-            <a href="homepage.html"><img src="Pictures/logo2.png" class="logo" ></a>
+            <a href="homepage.html"><img src="Pictures/logo2.png" class="logo"></a>
             <ul>
                 <li> <a href="home.php">Domov</a></li>
                 <li> <a href="$$$">Moduli</a></li>
@@ -21,15 +41,18 @@
             </ul>
         </div>
 
-        <div class="contentVP"> <h1> Vaš Profil </h1><br><br>
-            
-                                Ime: xxx <img src= "Pictures/edit.png" class="edit"></img><br><br>
-                                Password: *** <img src= "Pictures/edit.png" class="edit"></img><br><br>
-                                O meni:  <img src= "Pictures/edit.png" class="edit"></img><br><br>
-                                sngojgsdkogjoghjgldhkg </div>
+        <div class="contentVP">
+            <h1> Vaš Profil </h1><br><br>
 
-       
+            Ime: xxx <img src="Pictures/edit.png" class="edit"></img><br><br>
+            Password: *** <img src="Pictures/edit.png" class="edit"></img><br><br>
+            O meni: <img src="Pictures/edit.png" class="edit"></img><br><br>
+            sngojgsdkogjoghjgldhkg
+        </div>
+
+
     </div>
-    
+
 </body>
+
 </html>
