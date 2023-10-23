6124799b25fe8814f93ea56dba752bac1d0ee989
Zanono <supergerzan@gmail.com>
2023-10-23 11:54:44 +0200

    fwdfw
    sdfsdf

---
+++ Website/MojeOcene.php
@@ -0,0 +1,29 @@
+<?php
+include("Scripts/config.php");
+//START THE SESSION
+session_start();
+
+//if the user isn't logged in / session variables aren't set, redirect to login
+if (!isset($_SESSION["id"], $_SESSION["username"], $_SESSION["user_type"])) {
+    header("location: Scripts/logout.php");
+}
+//if the user is logged in, allow access
+else {
+    $username = $_SESSION["username"];
+    $id = $_SESSION["id"];
+    $user_type =  $_SESSION["user_type"];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text" href="Stylesheets/MojeOcene.css">
+    <link rel="stylesheet" type="text/css" href="Stylesheets/navbar.css">
+</head>
+<body>
+    
+</body>
+</html>
