dbe5422e6460c16cd32f0cf25173f3ffdf8082a3
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2023-10-24 20:22:29 +0200

    Bolj kot ne končan sub.php, manjkajo se podatki

--- Website/sub.php
+++ Website/sub.php
@@ -70,22 +70,149 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <?php
     if(isset($_GET['oddaja'])){
         $id_oddaja = $_GET['oddaja'];
-        /*$oddaja_query = "
-        SELECT `o`.*, `g`.*, `m`.*, `p`.*, `u`.*, `us`.*
+
+        //get submission data
+        $oddaja_query = "SELECT `o`.* , `p`.*, `g`.*
         FROM `oddaja` `o` JOIN `gradiva` `g`
             ON  `g`.`id_gradiva` = `o`.`id_gradiva` JOIN `model` `m`
             ON `m`.`id_modula` = `g`.`id_modula` JOIN `predmeti` `p`
-            ON `p`.`id_predmet` = `m`.`id_predmet` JOIN `ucilnica` `u`
-            ON `u`.`id_predmet` = `p`.`id_predmet` JOIN `user` `us`
-            ON `us`.`id_user` = `u`.`id_user`
-        WHERE `o`.`id_oddaja` = '$id_oddaja';
-        ";*/
-        $oddaja_query = "SELECT * FROM `oddaja` WHERE  `id_oddaja` = '$id_oddaja';";
+            ON `p`.`id_predmet` = `m`.`id_predmet`
+        WHERE `id_oddaja` = '$id_oddaja';";
         $oddaja_result = mysqli_query($db, $oddaja_query);
         $oddaja_count = mysqli_num_rows($oddaja_result);
         
+        //check if submission exists
         if($oddaja_count != 0){
-            
+            $oddaja_row = mysqli_fetch_assoc($oddaja_result);
+            $id_predmet = $oddaja_row['`p`.`id_predmet`'];
+
+            //query to see if user is a professor teaching the subject or admin
+            $prof_query = "SELECT `us`.*
+            FROM `user` `us` JOIN `ucilnica` `u`
+                ON `us`.`id_user` = `u`.`id_user` JOIN `predmeti` `p`
+                ON `u`.`id_predmet` = `p`.`id_predmet`
+            WHERE `us`.`id_user` = '$id' AND `u`.`user_type` = '1';
+            ";
+            $prof_result = mysqli_query($db, $prof_query);
+            $prof_count = mysqli_num_rows($prof_result);
+
+            //if rows are returned, display page
+            if($prof_count != 0 || $user_type == 0){
+                $user_data_query = "SELECT `us`.*, `o`.* 
+                FROM `user` `us` JOIN `oddaja` `o`
+                    ON `us`.`id_user` = `o`.`id_user`
+                WHERE `o`.`id_oddaja` = '$id_oddaja';
+                ";
+                $user_data_result = mysqli_query($db, $user_data_query);
+                $user_data_row = mysqli_fetch_assoc($user_data_result);
+
+                //TOP
+                echo"
+                <!--TOP-->
+                    <div class='top'>
+                        <!--PROFILE-->
+                        <div class='profile_con left'>
+                            <div>
+                                <img src='Pictures/Profile_Pictures/pfp_" . $user_data_row["`us`.`id_user`"] .".". $user_data_row["`us`.`img_ext`"] ."' class='pfp_img'> 
+                            </div>
+                            <div class='username'>". $user_data_row["`us`.`username`"] ."</div>     
+                        </div>
+                        <!--PROFILE-->
+
+                        <!--OCENA-->
+                        <div class='grade_con right'>
+                            <span class='title'>Ocena:</span> <input type='number' name='grade' class='grade_input' value='" . $user_data_row["`o`.`ocena`"] ."' max='100' maxlength='3' id='grade_input' onfocus='on_change(1)' oninput='on_change(1)' onblur='on_change(2)' required> / 100
+                        </div>
+                        <!--OCENA-->
+                    </div>
+                <!--TOP-->
+                ";
+                //TOP
+
+                //BOTTOM
+                echo"
+                <!--BOTTOM-->
+                    <div class='bottom'>
+                        <!--ODDAJA-->
+                        <div class='file_con left'>
+                            <div class='title'>
+                                Oddaja:
+                            </div>
+                            <div>
+                ";
+
+                //render different icons, depending on file extension
+                $file_ext = $user_data_row['`o`.`file_ext`'];
+                $file_name = $user_data_row["`us`.`ime`"] . $user_data_row["`us`.`priimek`"] . "-" . $oddaja_row['`g`.`naslov`'];
+
+                if($file_ext == "png"){
+                    echo"
+                            <div>
+                                <img src='Pictures/png_file.png' class='file_icon'><a href='Files/$id_oddaja.$file_ext' class='link' download='" .$file_name . "'>" . $file_name . "." . $file_ext . "</a>
+                            </div>
+                    ";
+                }
+                else if($file_ext == "jpg" || $file_ext == "jpeg" || $file_ext == "webp"){
+                    echo"
+                            <div>
+                                <img src='Pictures/img_file.png' class='file_icon'><a href='Files/$id_oddaja.$file_ext' class='link' download='" .$file_name . "'>" . $file_name . "." . $file_ext . "</a>
+                            </div>
+                    ";
+                }
+                else if($file_ext == "doc" || $file_ext == "docx"){
+                    echo"
+                            <div>
+                                <img src='Pictures/doc_file.png' class='file_icon'><a href='Files/$id_oddaja.$file_ext' class='link' download='" .$file_name . "'>" . $file_name . "." . $file_ext . "</a>
+                            </div>
+                    ";
+                }
+                else if($file_ext == "pdf"){
+                    echo"
+                            <div>
+                                <img src='Pictures/pdf_file.png' class='file_icon'><a href='Files/$id_oddaja.$file_ext' class='link' download='" .$file_name . "'>" . $file_name . "." . $file_ext . "</a>
+                            </div>
+                    ";
+                }
+                else if($file_ext == "zip" || $file_ext == "zipx" || $file_ext == "7z" || $file_ext == "rar" || $file_ext == "pak" || $file_ext == "rpm" || $file_ext == "gz"){
+                    echo"
+                            <div>
+                                <img src='Pictures/zip_file.png' class='file_icon'><a href='Files/$id_oddaja.$file_ext' class='link' download='" .$file_name . "'>" . $file_name . "." . $file_ext . "</a>
+                            </div>
+                    ";
+                }
+                else{
+                    echo"
+                            <div>
+                                <img src='Pictures/file_default.png' class='file_icon'><a href='Files/$id_oddaja.$file_ext' class='link' download='" .$file_name . "'>" . $file_name . "." . $file_ext . "</a>
+                            </div>
+                    ";
+                }
+
+                echo"
+                        <div>
+                            <input type='submit' value='Oceni' class='submit_btn'>
+                        </div>
+                    </div>
+                    <!--ODDAJA-->
+                ";
+
+                echo"
+                    <!--KOMENTAR-->
+                    <div class='comment_con right'>
+                        <span class='title'>Komentar:</span> <textarea name='comment' maxlength='100'>". $user_data_row['`o`.`komentar`'] ."</textarea>
+                    </div>
+                    <!--KOMENTAR-->
+                </div>
+                <!--BOTTOM-->
+                ";
+            }
+
+            //if not display error message
+            else{
+                echo"
+                <h1>Dostop prepovedan.</h1>
+                ";
+            }
         }
         else{
             header("location: class.php");
@@ -96,29 +223,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     ?>
 
-    <!--TOP-->
+    <!--TOP
         <div class="top">
-            <!--PROFILE-->
+            PROFILE
             <div class="profile_con left">
                 <div>
                     <img src="Pictures/stock_pfp.png" class="pfp_img"> 
                 </div>
                 <div class="username">Uporabniško ime</div>     
             </div>
-            <!--PROFILE-->
+            PROFILE
 
-            <!--OCENA-->
+            OCENA
             <div class="grade_con right">
                 <span class="title">Ocena:</span> <input type="number" name="grade" class="grade_input" value="90" max="100" maxlength="3" id='grade_input' onfocus="on_change(1)" oninput="on_change(1)" onblur="on_change(2)" required> / 100
             </div>
-            <!--OCENA-->
+            OCENA
         </div>
-    <!--TOP-->
+    TOP-->
 
 
-    <!--BOTTOM-->
+    <!--BOTTOM
         <div class="bottom">
-            <!--ODDAJA-->
+            ODDAJA
             <div class="file_con left">
                 <div class="title">
                     Oddaja:
@@ -130,15 +257,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <input type="submit" value="Oceni" class="submit_btn">
                 </div>
             </div>
-            <!--ODDAJA-->
+            ODDAJA
 
-            <!--KOMENTAR-->
+            KOMENTAR
             <div class="comment_con right">
                 <span class="title">Komentar:</span> <textarea name="comment" maxlength="100"></textarea>
             </div>
-            <!--KOMENTAR-->
+            KOMENTAR
         </div>
-    <!--BOTTOM-->
+    BOTTOM-->
     </form>
 </body>
 
