976b2ebba9026ee4138aaf8163aaad13dbd3a634
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2023-11-04 18:20:05 +0100

    delete.php - brisanje predmetov

--- Website/Scripts/delete.php
+++ Website/Scripts/delete.php
@@ -97,4 +97,101 @@ if($type == 2){
     }
 }
 
+//delete subject
+if($type == 3){
+    //check if user is an admin or student
+    if($user_type == 0 || $user_type == 2){
+        //check that class exists
+        $class_exists_query = "SELECT * FROM `predmeti`
+        WHERE `id_predmet` = '$get_id'";
+        $class_exists_result = mysqli_query($db, $class_exists_query);
+        $class_exists_count = mysqli_num_rows($class_exists_result);
+
+        //class exists
+        if($class_exists_count > 0){
+            //if the user is a student, drop subject
+            if($user_type == 2){
+                //check that the student is signed into the subject
+                $user_sign_query = "SELECT * FROM `ucilnica`
+                WHERE `id_predmet` = '$get_id' 
+                AND `id_user` = '$id';
+                ";
+                $user_sign_result = mysqli_query($db, $user_sign_query);
+                $user_sign_count = mysqli_num_rows($user_sign_result);
+
+                //student is signed up
+                if($user_sign_count > 0){
+                    //delete from oddaja
+                    $sub_del_query = "DELETE FROM `oddaja` WHERE `id_user` = '$id'";
+                    $sub_del_result = mysqli_query($db, $sub_del_query);
+
+                    //delete from ucilnica
+                    $sign_del_query = "DELETE FROM `ucilnica` 
+                    WHERE `id_user` = '$id' 
+                    AND `id_predmet` = '$get_id'";
+                    $sign_del_result = mysqli_query($db, $sign_del_query); 
+
+                    
+                }
+            }
+
+            //if the user is an admin, delete the subject alltogether
+            else if($user_type == 0){
+                //get all modules
+                $module_query = "SELECT * FROM `model` WHERE `id_predmet` = '$get_id'";
+                $module_result = mysqli_query($db, $module_query);
+                $module_count = mysqli_num_rows($module_result);
+
+                //if the subject has any modules continue through to oddaja table
+                if($module_count > 0){
+                    //loop through all modules
+                    while($mod_row = mysqli_fetch_assoc($module_result)){
+                        $gradiva_query = "SELECT * FROM `gradiva` 
+                        WHERE `id_modula` = '". $mod_row['id_modula'] ."'";
+                        $gradiva_result = mysqli_query($db, $gradiva_query);
+                        $gradiva_count = mysqli_num_rows($gradiva_result);
+
+                        //if there are materials under a module loop through them
+                        if($gradiva_count > 0){
+                            //loop through materials
+                            while($gradiva_row = mysqli_fetch_assoc($gradiva_result)){
+                                //delete submissions for each material
+                                $oddaja_query = "DELETE FROM `oddaja` 
+                                WHERE `id_gradiva` = '" . $gradiva_row['id_gradiva'] . "'";
+                                $oddaja_result = mysqli_query($db, $oddaja_query);
+                            }
+
+                            //delete materials for each module
+                            $gradiva_del_query = "DELETE FROM `gradiva` 
+                            WHERE `id_modula` = '" . $mod_row['id_modula'] . "'";
+                            $gradiva_del_result = mysqli_query($db, $gradiva_del_query);
+                        }
+                    }
+
+                    //delete modules of the subject
+                    $mod_del_query = "DELETE FROM `model` WHERE `id_predmet` = '$get_id'";
+                    $mod_del_result = mysqli_query($db, $mod_del_query);
+                }
+
+                //ucilnica table
+                $ucilnica_query = "SELECT * FROM `ucilnica` WHERE `id_predmet` = '$get_id'";
+                $ucilnica_result = mysqli_query($db, $ucilnica_query);
+                $ucilnica_count = mysqli_num_rows($ucilnica_result);
+
+                if($ucilnica_count > 0){
+                    $sign_del_query = "DELETE FROM `ucilnica` 
+                    WHERE `id_predmet` = '$get_id'";
+                    $sign_del_result = mysqli_query($db, $sign_del_query); 
+                }
+
+                //finally DELETE subject - predmeti table
+                $predmet_del_query = "DELETE FROM `predmeti` WHERE `id_predmet` = '$get_id';";
+                $predmet_del_result = mysqli_query($db, $predmet_del_query);
+            }
+        }   
+        
+    }
+    header("location: ../class.php");
+}
+
 ?>
